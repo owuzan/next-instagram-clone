6366d70d1cb556832228ca49228dd6f22075755f
Oğuzhan Yılmaz <ooguzhanyilmazz41@gmail.com>
2021-01-01 16:59:26 +0300

    For Vercel deploy error

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.scss'
-import Header from '../components/Header'
+import Header from '../components/Header/index'
 import Head from 'next/head'
 import Container from '../components/Container'
 
