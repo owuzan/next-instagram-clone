b51d09921ac5f5d4c79cac4f152d3f486f717ceb
Oğuzhan Yılmaz <ooguzhanyilmazz41@gmail.com>
2021-01-05 02:24:08 +0300

    Fixed .env

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        FIREBASE_API_KEY: 'AIzaSyD7pqsPi5LLq121Y5e4FbkI5RkPFW03bH4',
+        FIREBASE_AUTH_DOMAIN: 'instagram-clone-45093.firebaseapp.com',
+        FIREBASE_DATABASE_URL:
+            'https://instagram-clone-45093-default-rtdb.firebaseio.com',
+        FIREBASE_PROJECT_ID: 'instagram-clone-45093',
+        FIREBASE_STORAGE_BUCKET: 'instagram-clone-45093.appspot.com',
+        FIREBASE_MESSAGING_SENDER_ID: '820550670430',
+        FIREBASE_APP_ID: '1:820550670430:web:8aa8ca9925e5ca405bbccf',
+    },
+}
